e0dc56def2074c3dca2769a6b5e24f1f16c0c73f
krug <j.krug@lancaster.ac.uk>
2015-07-21 14:48:04 +0100

    A first stab at generalising post form data from a file.

--- openresty/post_form_data.lua
+++ openresty/post_form_data.lua
@@ -1,5 +1,7 @@
+
 cjson = require 'cjson'
 upload = require 'resty.upload'
+pg = require 'pgmoon'
 
 -- TODO: Needs some work to make this generic
 
@@ -44,17 +46,70 @@ function form_to_table()
     end
 end
 
-local t = form_to_table()
-local u = ngx.var['base_url'] ..
-    '/institution?' ..
-    '&inst_id=' .. t['inst_id'] ..
-    '&name=' .. ngx.escape_uri(t['name']) ..
-    '&contact=' .. ngx.escape_uri(t['contact']) ..
-    '&phone=' .. ngx.escape_uri(t['contact_phone']) ..
-    '&email=' .. ngx.escape_uri(t['contact_email']) ..
-    '&cris_sys=' .. ngx.escape_uri(t['cris_sys']) ..
-    '&pub_sys=' .. ngx.escape_uri(t['pub_sys']) ..
-    '&dataset_sys=' .. ngx.escape_uri(t['dataset_sys']) ..
-    '&archive_sys=' .. ngx.escape_uri(t['archive_sys'])
-local res = ngx.location.capture(u, {method=ngx.HTTP_POST})
-ngx.say(res.body)
+function get_connection_details()
+    local f = "/usr/local/openresty/lualib/connection.conf"
+    dofile(f)
+    return user, passwd
+end
+
+function open_dmaonline_db()
+    local u, p = get_connection_details()
+    local d = pg.new(
+        {
+            host="127.0.0.1",
+            port="5432",
+            database="DMAonline",
+            user=u, password=p
+        }
+    )
+    assert(d:connect())
+    return d
+end
+
+function close_dmaonline_db(c)
+    assert(c:disconnect())
+end
+
+function insert_institution_values(values)
+    local d = open_dmaonline_db()
+    local v = d:escape_literal(values['inst_id']) .. ', '
+    v = v .. d:escape_literal(values['name']) .. ', '
+    v = v .. d:escape_literal(values['contact']) .. ', '
+    v = v .. d:escape_literal(values['contact_phone']) .. ', '
+    v = v .. d:escape_literal(values['contact_email']) .. ', '
+    v = v .. d:escape_literal(values['cris_sys']) .. ', '
+    v = v .. d:escape_literal(values['pub_sys']) .. ', '
+    v = v .. d:escape_literal(values['dataset_sys']) .. ', '
+    v = v .. d:escape_literal(values['archive_sys']) .. ', '
+    v = v .. d:escape_literal(values['currency']) .. ', '
+    v = v .. d:escape_literal(values['currency_symbol'])
+    local q = "insert into institution values (" .. v .. ") returning *;"
+    local res = assert(d:query(q))
+    ngx.say(cjson.encode(res[1]))
+    close_dmaonline_db(d)
+end
+
+function insert_department_values(inst, values)
+    ngx.say('insert_department_values for ' .. inst)
+    tprint(values)
+end
+
+function insert_faculty_values(inst, values)
+    ngx.say('insert_faculty_values for ' .. inst)
+    tprint(values)
+end
+
+local inst = ngx.var.inst_id
+local object = ngx.var.object
+local object_function_table = {
+    department = insert_department_values,
+    faculty = insert_faculty_values
+}
+
+if object == "institution" then -- a special case
+    insert_institution_values(form_to_table())
+elseif object_function_table[object] then
+    object_function_table[object](inst, form_to_table())
+else
+    ngx.say("No function found for operation on " .. object)
+end
